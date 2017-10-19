1468a4d629d08eb6e6a98157d69f39aaef50b10c
Tonda Pleskac <pleskac@gmail.com>
2017-10-19 18:05:57 +0200

    Remove InstanceMethods module

--- lib/mongoid/fulltextable.rb
+++ lib/mongoid/fulltextable.rb
@@ -4,23 +4,16 @@ require 'active_support/concern'
 require 'core_ext/string'
 
 module Mongoid
-
   module Fulltextable
-
     extend ActiveSupport::Concern
 
     included do
-
       field :_keywords, :type => Array
-
       index :_keywords => 1
-
       scope :fulltext, lambda { |str| where(:_keywords => /#{Regexp.escape(str)}/i) }
-
     end
 
     module ClassMethods
-
       # Define fields which will be saved into full-text index.
       def fulltextable_fields(*args)
         before_save :update_keywords
@@ -34,58 +27,51 @@ module Mongoid
 
         self.instance_variable_set(:@index_proc, block)
       end
-
     end
 
-    module InstanceMethods
-
-      def update_keywords!
-        self.update_keywords
-        self.save!
-      end
-
-      protected
-
-      def update_keywords
-        update_keywords_from_proc if index_proc
-        update_keywords_from_fields if index_fields && !index_fields.empty?
-      end
-
-      def update_keywords_from_proc
-        self._keywords = []
-        instance_eval &index_proc
-      end
-
-      def update_keywords_from_fields
-        keywords = index_fields.map do |field|
-          value      = self.send(field).to_s
-          simplified = value.without_accents if value.respond_to?(:without_accents)
-
-          simplified == value ? value : [value, simplified]
-        end.reject(&:blank?).compact.flatten.uniq
-
-        self._keywords = keywords
-      end
-
-      def index_proc
-        @index_proc ||= self.class.instance_variable_get(:@index_proc)
-      end
-
-      def index_fields
-        @index_fields ||= self.class.instance_variable_get(:@index_fields)
-      end
-
-      def indexes(*args)
-        keywords = args.reject(&:blank?).compact.uniq.map do |value|
-          value      = value.to_s
-          simplified = value.without_accents if value.respond_to?(:without_accents)
-          simplified == value ? value : [value, simplified]
-        end
-        self._keywords += keywords.flatten.compact
-      end
-
+    def update_keywords!
+      self.update_keywords
+      self.save!
     end
 
+    protected
+
+    def update_keywords
+      update_keywords_from_proc if index_proc
+      update_keywords_from_fields if index_fields && !index_fields.empty?
+    end
+
+    def update_keywords_from_proc
+      self._keywords = []
+      instance_eval &index_proc
+    end
+
+    def update_keywords_from_fields
+      keywords = index_fields.map do |field|
+        value      = self.send(field).to_s
+        simplified = value.without_accents if value.respond_to?(:without_accents)
+
+        simplified == value ? value : [value, simplified]
+      end.reject(&:blank?).compact.flatten.uniq
+
+      self._keywords = keywords
+    end
+
+    def index_proc
+      @index_proc ||= self.class.instance_variable_get(:@index_proc)
+    end
+
+    def index_fields
+      @index_fields ||= self.class.instance_variable_get(:@index_fields)
+    end
+
+    def indexes(*args)
+      keywords = args.reject(&:blank?).compact.uniq.map do |value|
+        value      = value.to_s
+        simplified = value.without_accents if value.respond_to?(:without_accents)
+        simplified == value ? value : [value, simplified]
+      end
+      self._keywords += keywords.flatten.compact
+    end
   end
-
 end
